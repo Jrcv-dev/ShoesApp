c6fcc1798b1664f52d973625b290acb58262a9db
Curso <Curso@LSTK231299.stktrainning.com>
2019-12-10 10:06:16 -0600

    agregando entity framework al proyecto

---
+++ Shoes_Data/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shoes_Data
+{
+    public class CapaData
+    {
+        DataProductsEntities BDShoes = new DataProductsEntities();
+
+    }
+}
